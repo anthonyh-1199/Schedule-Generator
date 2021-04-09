ac6e991fba6cb455d64d3ec07d3b4cac1c97fddc
Anthony Howell <anthonyamh@gmail.com>
2021-04-09 10:47:52 -0500

    Created ScheduleLogic class

---
+++ src/schedulegenerator/ScheduleLogic.java
@@ -0,0 +1,63 @@
+/*
+ */
+
+package schedulegenerator;
+
+import java.lang.reflect.Array;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ScheduleLogic {
+    
+    HashMap<Integer, Employee> employeesMap = new HashMap<Integer, Employee>();
+    HashMap<Integer, Shift> shiftsMap = new HashMap<Integer, Shift>();
+    Database db;
+    
+    public ScheduleLogic(Database db){
+        this.db = db;
+        
+        for (int i = 1; i <= 6; i++){
+            Employee employee = db.getEmployee(i);
+            employeesMap.put(employee.getId(), employee);
+        }
+        
+        for (int i = 1; i <= 5; i++){
+            Shift shift = db.getShift(i);
+            shiftsMap.put(shift.getId(), shift);
+        }
+        
+    }
+    
+    public ArrayList<Employee> calcPotentialEmployees(int shiftId, ArrayList<Employee> currentEmployeesForDay){
+        //For each shift in the day, find somebody to work it
+        ArrayList<Employee> potentialEmployees = new ArrayList<Employee>(); 
+        ArrayList<Employee> scheduleArray = currentEmployeesForDay; 
+
+        //For each employee, choose the ones that can work the shift
+        for (int i = 1; i <= employeesMap.size(); i++){
+
+            //Get the current employee and his/her schedule
+            Employee e = employeesMap.get(i);
+            ArrayList<LocalTime[]> employeeSchedule = e.getSchedule();
+            
+            //Convert times into longs for comparisons
+            long shiftStartTime = shiftsMap.get(shiftId).getStartTime().toSecondOfDay();
+            long employeeStartTime = ((LocalTime)Array.get(employeeSchedule.get(0), 0)).toSecondOfDay();
+            long shiftEndTime = shiftsMap.get(shiftId).getEndTime().toSecondOfDay();
+            long employeeEndTime = ((LocalTime)Array.get(employeeSchedule.get(0), 1)).toSecondOfDay();
+            
+            //Choose employees who are available during that shift
+            if ((employeeStartTime <= shiftStartTime) && (employeeEndTime >= shiftEndTime)){
+                //Make sure to not include employees that already work that day
+                if (!scheduleArray.contains(e)){
+                    potentialEmployees.add(e);
+                }
+            
+            }
+        
+        }
+
+        return potentialEmployees;
+    }
+}
